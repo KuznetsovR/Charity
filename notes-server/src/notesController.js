fe05fd0e11ec6dc9973146699acad41c8f2afcf0
KuznetsovR <abcruslan2004@mail.ru>
2021-04-20 20:32:21 +0500

    architecture added

--- notes-server/src/notesController.js
+++ notes-server/src/notesController.js
@@ -1,22 +1,49 @@
 class NotesExpressController {
-    constructor(interactor) {
-        this.interactor = interactor;
+  constructor(interactor) {
+    this.interactor = interactor;
+  }
+  async getAll(req, res) {
+    const notes = await this.interactor.getNotes();
+    res.send(notes);
+  }
+  async addNote(req, res) {
+    try {
+      const note = await this.interactor.addNote(req.body);
+      res.status(201).send(note.id.toString());
+    } catch (e) {
+      if (e.message === "incorrect data") {
+        res.status(400).send();
+      } else {
+        throw e;
+      }
     }
-    async getAll(req, res) {
-        const notes = await this.interactor.getNotes();
-        res.send(notes)
+  }
+  deleteNote(req, res) {
+    try {
+      this.interactor.deleteNote(+req.params.id);
+      res.sendStatus(204);
+    } catch (e) {
+      if (e.message === "incorrect id") {
+        res.status(400).send();
+      } else {
+        throw e;
+      }
     }
-    async addNote(req, res) {
-        try {
-            const note = await this.interactor.addNote(req.body);
-            res.status(201).send(note.id.toString());
-        } catch(e){
-            if (e.message === 'incorrect data'){
-                res.status(400).send();
-            }else{
-                throw e;
-            }
-        }
+  }
+  editNote(req, res) {
+    try {
+      const id = +req.params.id;
+      const { heading, content } = req.body;
+      this.interactor.editNote({ heading, content, id });
+      console.log(1)
+      res.sendStatus(204);
+    } catch (e) {
+      if (e.message === "incorrect data" || e.message === "incorrect id") {
+        res.status(400).send();
+      } else {
+        throw e;
+      }
     }
+  }
 }
-module.exports = {NotesExpressController};
+module.exports = { NotesExpressController };
--- notes-server/src/notesInteractor.js
+++ notes-server/src/notesInteractor.js
@@ -1,16 +1,39 @@
-const isDefined = (val) => val !== undefined && val !== null && !(typeof value === 'number' && isNaN(val));
+const isDefined = (val) =>
+  val !== undefined &&
+  val !== null &&
+  !(typeof value === "number" && isNaN(val));
 class NotesInteractor {
-    constructor(repository) {
-        this.repository = repository;
+  constructor(repository) {
+    this.repository = repository;
+  }
+  getNotes() {
+    return this.repository.readAll();
+  }
+  addNote({ heading, content }) {
+    if (!heading || !isDefined(content)) {
+      throw Error("incorrect data");
     }
-    getNotes() {
-        return this.repository.readAll()
+    return this.repository.addNote({ heading, content });
+  }
+  deleteNote(id) {
+    if (!isDefined(id)) {
+      throw Error("incorrect id");
     }
-    addNote({ heading, content }) {
-        if (!heading || !isDefined(content)) {
-            throw Error('incorrect data')
-        }
-        return this.repository.addNote({ heading, content });
+    try {
+      this.repository.deleteNote({ id });
+    } catch (e) {
+      throw e;
     }
+  }
+  editNote({ heading, content, id }) {
+    if (!heading || !isDefined(content) || !isDefined(id)) {
+      throw Error("incorrect data or id");
+    }
+    try{
+        this.repository.editNote({ heading, content, id });
+    }catch (e) {
+      throw e;
+    }
+  }
 }
-module.exports = {NotesInteractor};
+module.exports = { NotesInteractor };
