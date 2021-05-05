4d5dd75d80b0a5d4e0fddf46fbc3fa2e2e249b2a
KuznetsovR <abcruslan2004@mail.ru>
2021-05-05 10:42:13 +0500

    UI

--- tilda2/src/app/app.module.ts
+++ tilda2/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PageRendererComponent } from './page-renderer/page-renderer.component';
 import { ElementRendererComponent } from './element-renderer/element-renderer.component';
 import { ElementOptionsComponent } from './element-options/element-options.component';
 import { ActiveElementService } from './services/active-element.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular//platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageRendererComponent,
     ElementRendererComponent,
-    ElementOptionsComponent
+    ElementOptionsComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [ActiveElementService],
   bootstrap: [AppComponent]
