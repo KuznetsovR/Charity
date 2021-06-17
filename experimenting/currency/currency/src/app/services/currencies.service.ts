dddab88db9867072f495587614b3fffd800e7a7d
KuznetsovR <abcruslan2004@mail.ru>
2021-06-17 22:21:17 +0500

    check

--- experimenting/currency/currency/src/app/services/currencies.service.ts
+++ experimenting/currency/currency/src/app/services/currencies.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { defaultCurrencies } from '../entities/mock';
 import { Currencies } from '../entities/types';
+import * as obj from '../asd'
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,22 @@ export class CurrenciesService {
   get currencies$(): Observable<Currencies> {
     return this._currencies$
   }
-  changeValue(value: number, rateToUsd: number){
-    this._currencies$.value.currentPriceInUsd = value * (1/rateToUsd)
+  changeValue(value: number, rateToUsd: number) {
+    this._currencies$.value.currentPriceInUsd = value * (1 / rateToUsd)
     // console.log(this._currencies$.value.currentPriceInUsd)
-    for (let currency of this._currencies$.value.currencies){
+    for (let currency of this._currencies$.value.currencies) {
       currency.value = +(this._currencies$.value.currentPriceInUsd * currency.rateToUsd).toFixed(2)
     }
   }
-  updateRates(){
-    fetch('http://www.floatrates.com/daily/usd.json', {method: "GET"}).then((res)=> res.json()).then((res)=> {
-    for (let currency of this._currencies$.value.currencies){
-      if (currency.name ==="USD") continue
-      currency.rateToUsd = res[currency.name.toLowerCase()].rate
-    }
-    console.log(this._currencies$.value)
-    this.changeValue(this._currencies$.value.currentPriceInUsd, 1)
-  })
+  updateRates() {
+    fetch('http://www.floatrates.com/daily/usd.json', { method: "GET" }).then((res) => res.json()).then((res) => {
+      for (let currency of this._currencies$.value.currencies) {
+        if (currency.name === "USD") continue
+        currency.rateToUsd = res[currency.name.toLowerCase()].rate
+      }
+      this.changeValue(this._currencies$.value.currentPriceInUsd, 1)
+    })
+    obj.func()
+    // uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD',
   }
 }
