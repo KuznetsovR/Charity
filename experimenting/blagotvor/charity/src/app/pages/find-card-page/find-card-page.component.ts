8729d4670edc0d6afc208c4fca154ccecc55804f
KuznetsovR <abcruslan2004@mail.ru>
2021-09-09 21:04:33 +0500

    added content to add, find and remove pages

--- experimenting/blagotvor/charity/src/app/pages/find-card-page/find-card-page.component.ts
+++ experimenting/blagotvor/charity/src/app/pages/find-card-page/find-card-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '../../interfaces/interfaces';
+import { STORES } from '../../constants/sections';
 
 @Component({
-  selector: 'app-find-card-page',
-  templateUrl: './find-card-page.component.html',
-  styleUrls: ['./find-card-page.component.scss']
+	selector: 'app-find-card-page',
+	templateUrl: './find-card-page.component.html',
+	styleUrls: ['./find-card-page.component.scss']
 })
 export class FindCardPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	selectedStore: Store | null = null;
+	stores = STORES;
+	constructor() {}
+	selectStore(store: Store): void {
+		this.selectedStore = store;
+	}
+	ngOnInit(): void {}
 }
--- experimenting/blagotvor/charity/src/app/pages/remove-page/remove-page.component.ts
+++ experimenting/blagotvor/charity/src/app/pages/remove-page/remove-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '../../interfaces/interfaces';
+import { STORES } from '../../constants/sections';
 
 @Component({
-  selector: 'app-remove-page',
-  templateUrl: './remove-page.component.html',
-  styleUrls: ['./remove-page.component.scss']
+	selector: 'app-remove-page',
+	templateUrl: './remove-page.component.html',
+	styleUrls: ['./remove-page.component.scss']
 })
 export class RemovePageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	selectedStore: Store | null = null;
+	stores = STORES;
+	constructor() {}
+	selectStore(store: Store): void {
+		this.selectedStore = store;
+	}
+	ngOnInit(): void {}
 }
