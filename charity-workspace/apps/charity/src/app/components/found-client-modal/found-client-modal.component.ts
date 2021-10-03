b0203fc1552e5696a2fefc77036581a71921bfdd
KuznetsovR <abcruslan2004@mail.ru>
2021-10-03 22:45:44 +0500

    found card and found client modals added

---
+++ charity-workspace/apps/charity/src/app/components/found-client-modal/found-client-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-found-client-modal',
+	templateUrl: './found-client-modal.component.html',
+	styleUrls: ['./found-client-modal.component.scss']
+})
+export class FoundClientModalComponent  {
+}
