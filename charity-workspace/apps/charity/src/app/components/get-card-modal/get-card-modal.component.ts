991c9f67e9c9fdf9bacc48684150e7b41ed372fe
KuznetsovR <abcruslan2004@mail.ru>
2021-10-03 22:47:21 +0500

    updated barcode and get card modals

--- charity-workspace/apps/charity/src/app/components/get-card-modal/get-card-modal.component.ts
+++ charity-workspace/apps/charity/src/app/components/get-card-modal/get-card-modal.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+// TODO: add 	const barcode = await this.apiService.getRequest(`path`); instead of mock
+//  to getCard when api is ready
+import { Component } from '@angular/core';
 import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 import { Store } from '../../interfaces/interfaces';
 import { STORES } from '../../constants/sections';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { BarcodeModalComponent } from '../barcode-modal/barcode-modal.component';
+import { ApiService } from '../../services/api.service';
 
 @Component({
 	selector: 'app-get-card-modal',
@@ -11,17 +14,24 @@ import { BarcodeModalComponent } from '../barcode-modal/barcode-modal.component'
 	styleUrls: ['./get-card-modal.component.scss'],
 	providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
-export class GetCardModalComponent{
+export class GetCardModalComponent {
 	selectedStore: Store | null = null;
 	stores = STORES;
 
-	constructor(private modalService: BsModalService, public bsModalRef: BsModalRef) {}
+	constructor(private modalService: BsModalService, public bsModalRef: BsModalRef, private apiService: ApiService) {}
 
 	selectStore(store: Store): void {
 		this.selectedStore = store;
 	}
-	getCard(): void {
+	async getCard(): Promise<void> {
 		this.bsModalRef.hide();
-		this.bsModalRef = this.modalService.show(BarcodeModalComponent, { class: 'modal-lg modal-dialog-centered' });
+		const barcode = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/UPC_A.svg/1200px-UPC_A.svg.png';
+		const initialState: ModalOptions = {
+			initialState: {
+				barcodeUrl: barcode,
+				class: 'modal-lg modal-dialog-centered'
+			}
+		};
+		this.bsModalRef = this.modalService.show(BarcodeModalComponent, initialState);
 	}
 }
