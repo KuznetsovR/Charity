8f14f99c5e9e8eb2ad6ff4c576c191c8bd4d23f3
KuznetsovR <abcruslan2004@mail.ru>
2021-10-22 15:44:31 +0500

    changed found card modal, made find btn in data table bigger, little store select redesign

--- charity-workspace/apps/charity/src/app/components/found-card-modal/found-card-modal.component.ts
+++ charity-workspace/apps/charity/src/app/components/found-card-modal/found-card-modal.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Card } from '../../interfaces/interfaces';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Card, FormControls, Store } from '../../interfaces/interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-found-card-modal',
@@ -7,6 +8,32 @@ import { Card } from '../../interfaces/interfaces';
 	styleUrls: ['./found-card-modal.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FoundCardModalComponent {
+export class FoundCardModalComponent implements OnInit {
 	data: Card;
+	dataState: 'changing' | 'static' = 'static';
+	selectedStore: Store;
+	changeClientForm: FormGroup;
+	cardNumber: FormControl;
+	id: FormControl;
+	owner: FormControl;
+	controls: FormControls = {};
+	ngOnInit(): void {
+		this.owner = new FormControl(this.data.owner, [Validators.required, Validators.pattern(/^[а-яё ]+$/i)]);
+		this.controls.owner = this.owner;
+		this.cardNumber = new FormControl(this.data.number, [Validators.required, Validators.pattern(/^\d{8,20}$/)]);
+		this.controls.cardNumber = this.cardNumber;
+		this.id = new FormControl(this.data.id, [Validators.required, Validators.pattern(/^\d{8,20}$/)]);
+		this.controls.id = this.id;
+		this.changeClientForm = new FormGroup(this.controls);
+		this.selectedStore = { name: this.data.shop, id: 1, image: '123' };
+	}
+	selectStore(store: Store): void {
+		this.selectedStore = store;
+	}
+	changeDataState(newState: 'changing' | 'static'): void {
+		this.dataState = newState;
+	}
+	changeCard(): void {
+		this.changeDataState('static');
+	}
 }
