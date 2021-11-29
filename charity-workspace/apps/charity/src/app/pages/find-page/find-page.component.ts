ae6bb9ffb5cedbaf2907bffcd94f165d24411c3a
KuznetsovR <abcruslan2004@mail.ru>
2021-11-29 15:32:23 +0500

    added getting real data from server to find page

--- charity-workspace/apps/charity/src/app/pages/find-page/find-page.component.ts
+++ charity-workspace/apps/charity/src/app/pages/find-page/find-page.component.ts
@@ -1,17 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Card, Client } from '../../interfaces/interfaces';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { getCardList, getClientList } from '../../actions/data-table.actions';
+import { ApiService } from '../../services/api.service';
 @Component({
 	selector: 'app-find-page',
 	templateUrl: './find-page.component.html',
 	styleUrls: ['./find-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FindPageComponent {
+export class FindPageComponent implements OnInit {
 	cards$: Observable<Card[]> = this.store.select('cards');
-	constructor(private store: Store<{ cards: Card[]; clients: Client[] }>) {}
 	clients$: Observable<Client[]> = this.store.select('clients');
 	cardKeys = ['Номер', 'Владелец', 'Магазин'];
 	clientKeys = ['Номер паспорта', 'ФИО'];
+	constructor(private store: Store<{ cards: Card[]; clients: Client[] }>, private apiService: ApiService) {}
+	ngOnInit(): void {
+		this.apiService.getRequest('admin/card').subscribe((data: Card[]) => {
+			this.store.dispatch(getCardList({ cards: data }));
+		});
+		this.apiService.getRequest('admin/owner').subscribe((data: Client[]) => {
+			this.store.dispatch(getClientList({ clients: data }));
+		});
+	}
 }
