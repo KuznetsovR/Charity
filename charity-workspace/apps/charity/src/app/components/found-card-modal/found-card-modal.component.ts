9cd819938f03bc7bb13c9e205742daf5700c6d9a
KuznetsovR <abcruslan2004@mail.ru>
2021-11-29 15:25:30 +0500

    updated found card modal

--- charity-workspace/apps/charity/src/app/components/found-card-modal/found-card-modal.component.ts
+++ charity-workspace/apps/charity/src/app/components/found-card-modal/found-card-modal.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Card, FormControls, Store } from '../../interfaces/interfaces';
+import { Card, Client, FormControls, Store as Shop } from '../../interfaces/interfaces';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { ApiService } from '../../services/api.service';
+import { getCardList } from '../../actions/data-table.actions';
+import { Store } from '@ngrx/store';
+import { mergeMap } from 'rxjs/operators';
 @Component({
 	selector: 'app-found-card-modal',
 	templateUrl: './found-card-modal.component.html',
@@ -9,31 +12,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FoundCardModalComponent implements OnInit {
+	constructor(private store: Store<{ cards: Card[]; clients: Client[] }>, private apiService: ApiService) {}
 	data: Card;
 	dataState: 'changing' | 'static' = 'static';
-	selectedStore: Store;
-	changeClientForm: FormGroup;
+	selectedStore: Shop;
+	changeCardForm: FormGroup;
 	cardNumber: FormControl;
-	id: FormControl;
-	owner: FormControl;
+	ownerId: FormControl;
 	controls: FormControls = {};
 	ngOnInit(): void {
-		this.owner = new FormControl(this.data.owner, [Validators.required, Validators.pattern(/^[а-яё ]+$/i)]);
-		this.controls.owner = this.owner;
-		this.cardNumber = new FormControl(this.data.number, [Validators.required, Validators.pattern(/^\d{8,20}$/)]);
+		this.ownerId = new FormControl(this.data.owner, [Validators.required, Validators.pattern(/^[а-яё ]+$/i)]);
+		this.controls.ownerId = this.ownerId;
+		this.cardNumber = new FormControl(this.data.cardNumber, [
+			Validators.required,
+			Validators.pattern(/^\d{8,20}$/)
+		]);
 		this.controls.cardNumber = this.cardNumber;
-		this.id = new FormControl(this.data.id, [Validators.required, Validators.pattern(/^\d{8,20}$/)]);
-		this.controls.id = this.id;
-		this.changeClientForm = new FormGroup(this.controls);
+		this.changeCardForm = new FormGroup(this.controls);
 		this.selectedStore = { name: this.data.shop, id: 1, image: '123' };
 	}
-	selectStore(store: Store): void {
+	selectStore(store: Shop): void {
 		this.selectedStore = store;
 	}
 	changeDataState(newState: 'changing' | 'static'): void {
 		this.dataState = newState;
 	}
 	changeCard(): void {
+		this.apiService
+			.putRequest('admin/card/put', {
+				id: this.data.id,
+				owner: this.data.owner,
+				cardNumber: this.cardNumber.value,
+				shop: this.selectedStore.name
+			})
+			.pipe(
+				mergeMap((res) => {
+					this.apiService.getRequest('admin/card').subscribe((data: Card[]) => {
+						this.store.dispatch(getCardList({ cards: data }));
+					});
+					return res;
+				})
+			)
+			.subscribe();
+		this.apiService.getRequest('admin/card').subscribe((data: Card[]) => {
+			this.store.dispatch(getCardList({ cards: data }));
+		});
 		this.changeDataState('static');
 	}
+	deleteCard(): void {}
 }
