ff36cc603c31a3145f2cfb532fcbf8bb5241d7a1
KuznetsovR <abcruslan2004@mail.ru>
2021-11-29 15:24:21 +0500

    added found client modal

--- charity-workspace/apps/charity/src/app/components/found-client-modal/found-client-modal.component.ts
+++ charity-workspace/apps/charity/src/app/components/found-client-modal/found-client-modal.component.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Client } from '../../interfaces/interfaces';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Card, Client, FormControls } from '../../interfaces/interfaces';
+import { ApiService } from '../../services/api.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { mergeMap } from 'rxjs/operators';
+import { getClientList } from '../../actions/data-table.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
 	selector: 'app-found-client-modal',
@@ -7,6 +12,77 @@ import { Client } from '../../interfaces/interfaces';
 	styleUrls: ['./found-client-modal.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FoundClientModalComponent {
+export class FoundClientModalComponent implements OnInit {
+	constructor(private store: Store<{ cards: Card[]; clients: Client[] }>, private apiService: ApiService) {}
 	data: Client;
+	dataState: 'changing' | 'static' = 'static';
+	changeClientForm: FormGroup;
+	useCount: FormControl;
+	passportNumber: FormControl;
+	name: FormControl;
+	surname: FormControl;
+	patronymic: FormControl;
+	controls: FormControls = {};
+	ngOnInit(): void {
+		this.useCount = new FormControl(this.data.useCount, [Validators.required, Validators.pattern(/^[0-9]*$/gm)]);
+		this.controls.useCount = this.useCount;
+		this.passportNumber = new FormControl(this.data.passportNumber, [
+			Validators.required,
+			Validators.pattern(/^\d{10}$/)
+		]);
+		this.controls.passportNumber = this.passportNumber;
+
+		this.name = new FormControl(this.data.name, [Validators.required, Validators.pattern(/^[а-яё]+$/i)]);
+		this.controls.name = this.name;
+		this.surname = new FormControl(this.data.surname, [Validators.required, Validators.pattern(/^[а-яё]+$/i)]);
+		this.controls.surname = this.surname;
+		this.patronymic = new FormControl(this.data.patronymic, [
+			Validators.required,
+			Validators.pattern(/^[а-яё]+$/i)
+		]);
+		this.controls.patronymic = this.patronymic;
+
+		this.changeClientForm = new FormGroup(this.controls);
+	}
+	changeDataState(newState: 'changing' | 'static'): void {
+		this.dataState = newState;
+	}
+	changeClient(): void {
+		this.apiService
+			.putRequest('admin/owner', {
+				id: this.data.id,
+				active: true,
+				useCount: this.useCount.value,
+				passportNumber: this.passportNumber.value,
+				name: this.name.value,
+				surname: this.surname.value,
+				patronymic: this.patronymic.value
+			})
+			.pipe(
+				mergeMap((res) => {
+					this.apiService.getRequest('admin/owner').subscribe((data: Client[]) => {
+						this.store.dispatch(getClientList({ clients: data }));
+					});
+					return res;
+				})
+			)
+			.subscribe();
+		this.changeDataState('static');
+	}
+	deleteCard(): void {
+		this.apiService
+			.putRequest('admin/owner', {
+				...this.data,
+				active: false
+			})
+			.pipe(
+				mergeMap((res) => {
+					this.apiService.getRequest('admin/owner').subscribe((data: Client[]) => {
+						this.store.dispatch(getClientList({ clients: data }));
+					});
+					return res;
+				})
+			)
+			.subscribe();
+	}
 }
