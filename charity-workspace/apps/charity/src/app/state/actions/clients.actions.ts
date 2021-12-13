0dbdf5bcdfd8c0914fdd87579839b23a5f0d9c08
KuznetsovR <abcruslan2004@mail.ru>
2021-12-13 15:38:08 +0500

    updated clients actions to add effect later

--- charity-workspace/apps/charity/src/app/state/actions/clients.actions.ts
+++ charity-workspace/apps/charity/src/app/state/actions/clients.actions.ts
@@ -1,10 +1,16 @@
 import { createAction, props } from '@ngrx/store';
 import { Client } from '../../interfaces/client.entity';
+import { QueryParameters } from '../../interfaces/queryParameters';
 
 export const getClientList = createAction(
-	'[Client List/API] Retrieve clients Success',
-	props<{ clients: readonly Client[] }>()
+	'[Client List/API] Retrieve clients',
+	props<{ parameters: QueryParameters; setNewParams: boolean }>()
 );
+export const getClientListSuccess = createAction(
+	'[Client List/API] Retrieve clients Success',
+	props<{ cards: readonly Client[] }>()
+);
+export const getClientListError = createAction('[Client List/API] Retrieve clients failed');
 
 export const addClient = createAction('[Client List] Add client', props<{ client: Client }>());
 export const removeClient = createAction('[Client List] Remove client', props<{ client: Client }>());
