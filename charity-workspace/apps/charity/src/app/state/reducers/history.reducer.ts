75ef82586d0f20d7ff7a3c20578ec471e4a41a69
KuznetsovR <abcruslan2004@mail.ru>
2021-12-13 15:30:56 +0500

    added generic to history reducer creation

--- charity-workspace/apps/charity/src/app/state/reducers/history.reducer.ts
+++ charity-workspace/apps/charity/src/app/state/reducers/history.reducer.ts
@@ -4,7 +4,7 @@ import { HistoryAction } from '../../interfaces/historyAction';
 
 export const initialState: readonly HistoryAction[] = [];
 
-export const historyReducer = createReducer(
+export const historyReducer = createReducer<readonly HistoryAction[]>(
 	initialState,
 	on(getHistory, (state, { history }) => history)
 );
