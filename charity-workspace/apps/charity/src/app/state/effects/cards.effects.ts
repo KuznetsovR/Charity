bb9b431a25ff1c86b9b7c870839552d82536b452
KuznetsovR <abcruslan2004@mail.ru>
2021-12-13 15:30:08 +0500

    added cards effects for updating

---
+++ charity-workspace/apps/charity/src/app/state/effects/cards.effects.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { ApiService } from '../../services/api.service';
+import { getCardList, getCardListError, getCardListSuccess } from '../actions/cards.actions';
+import { Card } from '../../interfaces/card.entity';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ErrorModalComponent } from '../../components/error-modal/error-modal.component';
+
+@Injectable()
+export class CardsEffects {
+	loadCards$ = createEffect(() =>
+		this.actions$.pipe(
+			ofType(getCardList),
+			mergeMap((action) =>
+				this.apiService.getRequest(`admin/card`, action.parameters, action.setNewParams).pipe(
+					map((cards: Card[]) => {
+						return getCardListSuccess({ cards });
+					}),
+					catchError(() => {
+						const initialState: ModalOptions = {
+							class: 'modal-dialog-centered',
+							animated: true
+						};
+						this.modalService.show(ErrorModalComponent, initialState);
+						return of(getCardListError());
+					})
+				)
+			)
+		)
+	);
+
+	constructor(
+		private actions$: Actions,
+		private apiService: ApiService,
+		private modalService: BsModalService
+	) {}
+}
