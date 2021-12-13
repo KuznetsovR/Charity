febd1e7030787dd0b54a74ba7866c80ad18b5d85
KuznetsovR <abcruslan2004@mail.ru>
2021-12-13 15:26:40 +0500

    added queryParameters interface

---
+++ charity-workspace/apps/charity/src/app/interfaces/queryParameters.ts
@@ -0,0 +1,10 @@
+export interface QueryParameters {
+	owner?: string;
+	number?: number;
+	passport?: number;
+	name?: string;
+	surname?: string;
+	patronymic?: string;
+	cardId?: number;
+	customerId?: number;
+}
