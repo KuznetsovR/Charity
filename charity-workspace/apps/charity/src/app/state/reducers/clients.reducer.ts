2cd2c61800b127ca4dac23f84d169cff6c01f4b8
KuznetsovR <abcruslan2004@mail.ru>
2021-12-13 15:47:19 +0500

    added new actions to clients.reducer.ts

--- charity-workspace/apps/charity/src/app/state/reducers/clients.reducer.ts
+++ charity-workspace/apps/charity/src/app/state/reducers/clients.reducer.ts
@@ -1,12 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
 import { Client } from 'src/app/interfaces/client.entity';
-import { addClient, getClientList, removeClient } from '../actions/clients.actions';
+import { addClient, getClientsListError, getClientsListSuccess, removeClient } from '../actions/clients.actions';
 
 export const initialState: readonly Client[] = [];
 
 export const clientsReducer = createReducer<readonly Client[]>(
 	initialState,
-	on(getClientList, (state, { clients }) => clients),
-	on(addClient, (state, { client }) => [...state, client]),
-	on(removeClient, (state, { client }) => state.filter((el) => el.passportNumber !== client.passportNumber))
+	on(getClientsListError, (state) => state),
+	on(getClientsListSuccess, (state, { clients }) => clients),
+	on(addClient, (state, { clients }) => [...state, clients]),
+	on(removeClient, (state, { clients }) => state.filter((el) => el.passportNumber !== clients.passportNumber))
 );
