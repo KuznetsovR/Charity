0d2c1fc12dd4f528eb457b882638c4b45b4ba835
KuznetsovR <abcruslan2004@mail.ru>
2021-12-15 17:31:28 +0500

    added spinner and not found rows to table

--- apps/charity/src/app/components/data-table/data-table.component.ts
+++ apps/charity/src/app/components/data-table/data-table.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { FoundCardModalComponent } from '../found-card-modal/found-card-modal.component';
 import { FoundClientModalComponent } from '../found-client-modal/found-client-modal.component';
 import { SearchModalComponent } from '../search-modal/search-modal.component';
-import { Observable } from 'rxjs';
 import { Card } from 'src/app/interfaces/card.entity';
 import { Client } from 'src/app/interfaces/client.entity';
 import { HistoryAction } from '../../interfaces/historyAction';
@@ -14,10 +13,11 @@ import { HistoryAction } from '../../interfaces/historyAction';
 	styleUrls: ['./data-table.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DataTableComponent {
-	@Input() data: Observable<Card[]> | Observable<Client[]> | Observable<HistoryAction[]>;
+export class DataTableComponent implements OnChanges {
+	@Input() data: Card[] | Client[] | HistoryAction[];
 	@Input() dataKeys: string[];
 	@Input() dataType: string;
+	isStateInitial = true;
 	bsModalRef?: BsModalRef;
 	constructor(private modalService: BsModalService) {}
 	openSearchModal(): void {
@@ -29,6 +29,11 @@ export class DataTableComponent {
 			}
 		};
 		this.bsModalRef = this.modalService.show(SearchModalComponent, initialState);
+	}
+	ngOnChanges(changes: SimpleChanges): void {
+		if (!changes.data.isFirstChange()) {
+			this.isStateInitial = false;
+		}
 	}
 
 	openModal(data: Card | Client): void {
